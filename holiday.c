d034734f14677b9934b179d06e8ee2cdda031b64
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-02-22 15:35:37 +0530

    holiday.c

---
+++ holiday.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+int i;
+char a[100];
+scanf("%c",&a);
+    if(a[0]=='S'||a[0]=='s')
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
